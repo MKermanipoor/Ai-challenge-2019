ebe97716cb6ccf76fcc4821e71a181e8abd9c19f
Mahdi <m1a3h7d5i@gmail.com>
2019-02-28 15:53:20 +0330

    ok kardan maghsada

--- AIC19-Client-Java-1.1.1/src/client/logicAI/PreProcessIMP.java
+++ AIC19-Client-Java-1.1.1/src/client/logicAI/PreProcessIMP.java
@@ -4,14 +4,16 @@ import client.model.Cell;
 import client.model.Map;
 import client.model.World;
 
-import java.security.Key;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
 import java.util.Set;
 
 public class PreProcessIMP implements PreProcess {
     private HashMap<String, Cell> cellLocation = new HashMap<>();
-    private HashMap<Cell, Integer> distanceHashMap = new HashMap<>();
+    private HashMap<Cell, Integer> distanceMyHashMap = new HashMap<>();
+    private HashMap<Cell, Integer> distanceOppHashMap = new HashMap<>();
+    private ArrayList<Cell> inUseCell=new ArrayList<>(4);
     private final World world;
     private int row, column;
 
@@ -38,52 +40,44 @@ public class PreProcessIMP implements PreProcess {
             }
             row = Math.round(row / myHeroCell.length);
             column = Math.round(column / myHeroCell.length);
+
+            //opp
+            int oppRow = 0;
+            int oppColumn = 0;
+            for (Cell workCell :
+                    map.getOppRespawnZone()) {
+                oppRow += workCell.getRow();
+                oppColumn += workCell.getColumn();
+            }
+            oppRow = Math.round(oppRow / map.getOppRespawnZone().length);
+            oppColumn = Math.round(oppColumn / map.getOppRespawnZone().length);
+
             //find best goal cell
             firstBestCell = goalCell[0];
             distance = getDistance(firstBestCell);
-            distanceHashMap.put(firstBestCell, distance);
+            distanceMyHashMap.put(firstBestCell, distance);
             for (int i = 1; i < goalCell.length; i++) {
                 Cell workCell = goalCell[i];
                 int d = getDistance(workCell);
-                distanceHashMap.put(workCell, d);
+                distanceMyHashMap.put(workCell, d);
+                distanceOppHashMap.put(workCell, world.getPathMoveDirections(oppRow,oppColumn, workCell.getRow(), workCell.getColumn()).length);
                 if (distance > d) {
                     firstBestCell = workCell;
-                    distance=d;
+                    distance = d;
                 }
             }
             Cell secondBestCell = null;
             //firstBestCell is helper first location
-//        findHelarLocation:for (int j = 4; j >0; j--) {
-//            for (int i = j; i > 0 ; i--) {
-//                int k=j-i;
-//                if(getCellBy(firstBestCell,i,k).isInObjectiveZone()){
-//                    secondBestCell=getCellBy(firstBestCell,i,k);
-//                    break findHelarLocation;
-//                }else if (getCellBy(firstBestCell,-i,k).isInObjectiveZone()){
-//                    secondBestCell=getCellBy(firstBestCell,-i,k);
-//                    break findHelarLocation;
-//                }else if(getCellBy(firstBestCell,i,-k).isInObjectiveZone()){
-//                    secondBestCell=getCellBy(firstBestCell,i,-k);
-//                    break findHelarLocation;
-//                }else if (getCellBy(firstBestCell,-i,-k).isInObjectiveZone()){
-//                    secondBestCell=getCellBy(firstBestCell,-i,-k);
-//                    break findHelarLocation;
-//                }
-//            }
-//        }
-//        if (secondBestCell==null){
             Random random = new Random();
-            if (random.nextBoolean()) {
-                secondBestCell = checkIsWall(getCellBy(firstBestCell, 2, 0));
-            } else {
-                secondBestCell = checkIsWall(getCellBy(firstBestCell, -2, 0));
-            }
-            //        }
+//            if (random.nextBoolean()) {
+//                secondBestCell = checkIsWall(getCellBy(firstBestCell, 2, 0));
+//            } else {
+//                secondBestCell = checkIsWall(getCellBy(firstBestCell, -2, 0));
+//            }
+            secondBestCell=checkIsWallMin(firstBestCell,1);
             //Tonk
-            Cell firstTonk = checkIsWall(getCellBy(firstBestCell, 0, 2));
-            Cell secondTonk = checkIsWall(getCellBy(secondBestCell, 0, 2));
-
-
+            Cell firstTonk = checkIsWallMax(firstBestCell,  1);
+            Cell secondTonk = checkIsWallMax(secondBestCell,1);
             //hasVariable
             cellLocation.put(Values.HEALER_1, firstBestCell);
             cellLocation.put(Values.HEALER_2, secondBestCell);
@@ -92,40 +86,92 @@ public class PreProcessIMP implements PreProcess {
             printHash();
         }
         return cellLocation;
-
     }
 
+
+
     private void printHash() {
-        Set<String> keys=cellLocation.keySet();
+        Set<String> keys = cellLocation.keySet();
         for (String key :
                 keys) {
-            System.out.println(key+" in :( "+cellLocation.get(key).getRow()+" , "+cellLocation.get(key).getColumn()+" )");
+            System.out.println(key + " in :( " + cellLocation.get(key).getRow() + " , " + cellLocation.get(key).getColumn() + " )");
         }
     }
 
-    private Cell checkIsWall(Cell secondBestCell) {
-        if (secondBestCell.isWall()) {
-            findHelarLocation:
-            for (int j = 1; j < 5; j++) {
+    private Cell checkIsWallMin(Cell secondBestCell,int minDistans) {
+//        if (!secondBestCell.isInObjectiveZone()) {
+//            findHelarLocation:
+        Cell maghsad=secondBestCell;
+            for (int j = minDistans; j < minDistans+2; j++) {
                 for (int i = j; i > 0; i--) {
                     int k = j - i;
-                    if (getCellBy(secondBestCell, i, k).isInObjectiveZone()) {
-                        secondBestCell = getCellBy(secondBestCell, i, k);
-                        break findHelarLocation;
-                    } else if (getCellBy(secondBestCell, -i, k).isInObjectiveZone()) {
-                        secondBestCell = getCellBy(secondBestCell, -i, k);
-                        break findHelarLocation;
-                    } else if (getCellBy(secondBestCell, i, -k).isInObjectiveZone()) {
-                        secondBestCell = getCellBy(secondBestCell, i, -k);
-                        break findHelarLocation;
-                    } else if (getCellBy(secondBestCell, -i, -k).isInObjectiveZone()) {
-                        secondBestCell = getCellBy(secondBestCell, -i, -k);
-                        break findHelarLocation;
+                    int d=Integer.MIN_VALUE;
+                    if (checkCell(getCellBy(secondBestCell, i, k))&&d<distanceOppHashMap.get(getCellBy(secondBestCell, i, k))) {
+                        maghsad = getCellBy(secondBestCell, i, k);
+                        d=distanceOppHashMap.get(getCellBy(secondBestCell, i, k));
+//                        break findHelarLocation;
+                    }
+                    if (checkCell(getCellBy(secondBestCell, -i, k))&&d<distanceOppHashMap.get(getCellBy(secondBestCell, -i, k))) {
+                        maghsad = getCellBy(secondBestCell, -i, k);
+                        d=distanceOppHashMap.get(getCellBy(secondBestCell, -i, k));
+//                        break findHelarLocation;
+                    }
+                    if (checkCell(getCellBy(secondBestCell, i, -k))&&d<distanceOppHashMap.get(getCellBy(secondBestCell, i, -k))) {
+                        maghsad = getCellBy(secondBestCell, i, -k);
+                        d=distanceOppHashMap.get(getCellBy(secondBestCell, i, -k));
+//                        break findHelarLocation;
+                    }
+                    if (checkCell(getCellBy(secondBestCell, -i, -k))&&d<distanceOppHashMap.get(getCellBy(secondBestCell, -i, -k))) {
+                        maghsad = getCellBy(secondBestCell, -i, -k);
+                        d=distanceOppHashMap.get(getCellBy(secondBestCell, -i, -k));
+//                        break findHelarLocation;
                     }
                 }
             }
+//        }
+        inUseCell.add(maghsad);
+        return maghsad;
+    }
+
+    private Cell checkIsWallMax(Cell secondBestCell,int minDistans) {
+//        if (!secondBestCell.isInObjectiveZone()) {
+//            findHelarLocation:
+        Cell maghsad=secondBestCell;
+        for (int j = minDistans; j < minDistans+2; j++) {
+            for (int i = j; i > 0; i--) {
+                int k = j - i;
+                int d=Integer.MAX_VALUE;
+                if (
+                        checkCell(getCellBy(secondBestCell, i, k))
+                        &&d>distanceOppHashMap.get(getCellBy(secondBestCell, i, k))) {
+                    maghsad = getCellBy(secondBestCell, i, k);
+                    d=distanceOppHashMap.get(getCellBy(secondBestCell, i, k));
+//                        break findHelarLocation;
+                }
+                if (checkCell(getCellBy(secondBestCell, -i, k))&&d>distanceOppHashMap.get(getCellBy(secondBestCell, -i, k))) {
+                    maghsad = getCellBy(secondBestCell, -i, k);
+                    d=distanceOppHashMap.get(getCellBy(secondBestCell, -i, k));
+//                        break findHelarLocation;
+                }
+                if (checkCell(getCellBy(secondBestCell, i, -k))&&d>distanceOppHashMap.get(getCellBy(secondBestCell, i, -k))) {
+                    maghsad = getCellBy(secondBestCell, i, -k);
+                    d=distanceOppHashMap.get(getCellBy(secondBestCell, i, -k));
+//                        break findHelarLocation;
+                }
+                if (checkCell(getCellBy(secondBestCell, -i, -k))&&d>distanceOppHashMap.get(getCellBy(secondBestCell, -i, -k))) {
+                    maghsad = getCellBy(secondBestCell, -i, -k);
+                    d=distanceOppHashMap.get(getCellBy(secondBestCell, -i, -k));
+//                        break findHelarLocation;
+                }
+            }
         }
-        return secondBestCell;
+//        }
+        inUseCell.add(maghsad);
+        return maghsad;
+    }
+
+    private boolean checkCell(Cell cellBy) {
+        return cellBy.isInObjectiveZone()&&!inUseCell.contains(cellBy);
     }
 
     private Cell getCellBy(Cell goalCell, int row, int column) {
